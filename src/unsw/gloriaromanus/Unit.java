53ef2305efcb40610657b5a1bf06aaf1f7b6933c
Kennan <wongkennan@gmail.com>
2020-10-24 23:20:30 +1100

    created configuration file for units and updates unit class

--- src/unsw/gloriaromanus/Unit.java
+++ src/unsw/gloriaromanus/Unit.java
@@ -1,5 +1,24 @@
 package unsw.gloriaromanus;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Represents a basic unit of soldiers
  * 
@@ -10,6 +29,7 @@ package unsw.gloriaromanus;
  */
 public class Unit {
     private int numTroops;  // the number of troops in this unit (should reduce based on depletion)
+    private String category;
     private int range;  // range of the unit
     private int armour;  // armour defense
     private int morale;  // resistance to fleeing
@@ -18,21 +38,25 @@ public class Unit {
     private int defenseSkill;  // skill to defend in battle. Does not protect from arrows!
     private int shieldDefense; // a shield
 
-    public Unit(){
+    public Unit(String name, int numTroops) throws IOException {
         // TODO = obtain these values from the file for the unit
-        numTroops = 50;
-        range = 1;
-        armour = 5;
-        morale = 10;
-        speed = 10;
-        attack = 6;
-        defenseSkill = 10;
-        shieldDefense = 3;
+        String unitConfigurationContent = Files.readString(Paths.get("src/unsw/gloriaromanus/unit_configuration.json"));
+        JSONObject unitConfiguration = new JSONObject(unitConfigurationContent);
+        JSONObject unitStat = unitConfiguration.getJSONObject(name);
+        this.numTroops = numTroops;
+        this.category = unitStat.getString("category");
+        this.range = Integer.parseInt(unitStat.getString("range"));
+        armour = Integer.parseInt(unitStat.getString("armour"));
+        morale = Integer.parseInt(unitStat.getString("morale"));
+        speed = Integer.parseInt(unitStat.getString("speed"));
+        attack = Integer.parseInt(unitStat.getString("attack"));
+        defenseSkill = Integer.parseInt(unitStat.getString("defenseSkill"));
+        shieldDefense = Integer.parseInt(unitStat.getString("shieldDefense"));
     }
 
     public int getNumTroops(){
         return numTroops;
     }
-
+    
     
 }
