ccfb4c33728580b2025981350add8aa8c09f16df
comp2511-student <jtran20122000@hotmail.com>
2020-10-28 21:44:07 -0700

    initial commit for beginning wealth and taxes

---
+++ src/unsw/gloriaromanus/economy/ProvinceWealth.java
@@ -0,0 +1,6 @@
+package unsw.gloriaromanus.economy;
+
+public class ProvinceWealth {
+    //Observer pattern
+    //Observes
+}
---
+++ src/unsw/gloriaromanus/economy/Treasury.java
@@ -0,0 +1,16 @@
+package unsw.gloriaromanus.economy;
+
+public class Treasury {
+    //actual money that can be spent
+    //function of province wealth and tax rate
+    //updates accordingly
+
+
+
+    //update function checks the province's commands for that round
+    public void update() {
+        //check with the other 
+    }
+}
+
+
